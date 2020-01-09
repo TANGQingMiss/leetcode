bf135fdfda84c12853e4d4628281b17b51c9d00c
TANG Qing <tangqingmiss@163.com>
2020-01-09 10:33:04 +0800

    verify if a tree is symmetric by reccurance

---
+++ isSymmetricTree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(root == null) return true;
+    return dfs(root.left,root.right);
+};
+
+function dfs(root1,root2){
+    if(root1 == null && root2 == null) return true;
+    if(root1 == null || root2 == null) return false;   
+    if(root1.val !== root2.val) return false;
+    return dfs(root1.left,root2.right) && dfs(root1.right,root2.left);
+}
