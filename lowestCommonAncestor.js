0e5684735cce6458a98f8eb8286ce13b85bc74f7
TANG Qing <tangqingmiss@163.com>
2020-02-01 12:34:13 +0800

    find the nearest ancestor

---
+++ lowestCommonAncestor.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(p.val == root.val){
+        return p;
+    }
+    if(q.val == root.val){
+        return q;
+    }
+    if(p.val > root.val && q.val > root.val){
+        return lowestCommonAncestor(root.right,p,q);
+    }else if(p.val < root.val && q.val < root.val){
+        return lowestCommonAncestor(root.left,p,q);
+    }else{
+        return root;
+    }
+};
