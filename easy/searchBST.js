95b79b202c96e060d7a73e6cf459c81abe290772
TANG Qing <tangqingmiss@163.com>
2020-02-11 11:43:04 +0800

    search BST by recursion

---
+++ easy/searchBST.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(root == null || root.val == val) return root;
+    return root.val > val ? searchBST(root.left,val) : searchBST(root.right,val);
+};
