3cb041d20d68f8d4f2eb9d8e1721e2d47151fc96
TANG Qing <tangqingmiss@163.com>
2020-02-19 13:39:22 +0800

    move chips to get minimum cost

---
+++ easy/minCostoMoveChips.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} chips
+ * @return {number}
+ */
+var minCostToMoveChips = function(chips) {
+    let odd = 0;
+    let even = 0;
+    for(let i = 0; i < chips.length; i++){
+        if(chips[i] % 2 == 0) even++;
+        else odd++;
+    }
+    return Math.min(odd,even);
+};
