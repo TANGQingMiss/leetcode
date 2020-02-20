73993f1cadc05f444a5ec521e27261813e78f235
TANG Qing <tangqingmiss@163.com>
2020-02-20 17:11:56 +0800

    remove palindrome

---
+++ easy/removePalindromeSubSequence.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function(s) {
+    if(s == "") return 0;
+    for(let i = 0, j = s.length - 1; i < j; i++,j--){
+        if(s[i] != s[j]) return 2;
+    }
+    return 1;
+};
