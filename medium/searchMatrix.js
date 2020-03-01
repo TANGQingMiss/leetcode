8ba30de0a56a33b997c4760d360aea403aa30602
TANG Qing <tangqingmiss@163.com>
2020-03-01 14:12:02 +0800

    search a number in ordered matrix

---
+++ medium/searchMatrix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if(matrix.length == 0) return false;
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while(row < matrix.length && col >= 0){
+        if(matrix[row][col] < target){
+            row++;
+        }else if(matrix[row][col] > target){
+            col--;
+        }else return true;
+    }
+    return false;
+};
