3e1b58eabf01251c9f1b1516401040cf35ff437f
TANG Qing <tangqingmiss@163.com>
2020-04-04 14:48:00 +0800

    check wiggle array

---
+++ medium/wiggleMaxLength.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var wiggleMaxLength = function(nums) {
+    let n = nums.length;
+    if(n < 2) return n;
+    let up = 1;
+    let down = 1;
+    for(let i = 1; i < n; i++){
+        if(nums[i] > nums[i-1]){
+            up = down + 1;
+        }
+        if(nums[i] < nums[i-1]){
+            down = up + 1;
+        }
+    }
+    return Math.max(up,down);
+};
