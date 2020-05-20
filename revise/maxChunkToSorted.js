a816c2a9a53a0318dec309f6665969aaf5a895aa
TANG Qing <tangqingmiss@163.com>
2020-05-20 20:10:17 +0800

    divide array and sort separately to make sorted array

---
+++ revise/maxChunkToSorted.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+//数组arr是[0, 1, ..., arr.length - 1]的一种排列，我们将这个数组分割成几个“块”，并将这些块分别进行排序。之后再连接起来，使得连接的结果和按升序排序后的原数组相同
+var maxChunksToSorted = function(arr) {
+        //当遍历到第i个位置时，如果可以切分为块，那前i个位置的最大值一定等于i。
+        //否则，一定有比i小的数划分到后面的块，那块排序后，一定不满足升序。
+        let res = 0, max = 0;
+        for (let i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);//统计前i个位置的最大元素
+            if (max == i) res++;
+        }
+        return res;
+};
